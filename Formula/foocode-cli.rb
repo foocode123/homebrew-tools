5928c733f9fa19fa52aeb5869025fce18fdd0c47
Noushad <test@foocode.com>
2024-06-30 13:19:57 +0000

    Brew formula update for foocode-cli version 3.1.5

--- Formula/foocode-cli.rb
+++ Formula/foocode-cli.rb
@@ -1,12 +1,17 @@
-class VeracodeCli < Formula
-  desc "You use the Veracode CLI to perform various actions for testing the security of your applications."
+class veracodeCli < Formula
+  desc "You use the veracode CLI to perform various actions for testing the security of your applications."
   homepage "https://www.veracode.com"
-  version "1.0.48"
+  version "3.1.5"
   license "MIT"
 
   on_macos do
-    url "https://tools.veracode.com/veracode-cli/veracode-cli_1.0.48_macosx_arm64.tar.gz"
-    sha256 "f04bafe166f4b8b93a029524214bc36c4950d540aa12c3b1be4ad911d74b5944"
+    if Hardware::CPU.arm?
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_3.1.5_macosx_arm64.tar.gz"
+      sha256 "d77b2aa19842b67f4b5a509300b6afbe7c8fbb934afbd932392b1fd457756fb2"
+    elsif Hardware::CPU.intel?
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_3.1.5_macosx_x86.tar.gz"
+      sha256 "68bdf646faeebe0a97e41e83d65c00b7812b87a54b62b3aae865a3b5ca768c0b"
+    end
 
     def install
       bin.install "veracode"
@@ -15,16 +20,16 @@ class VeracodeCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_1.0.48_linux_x86.tar.gz"
-      sha256 "3972e1abd3290604b82ca62da4f3b4bfe3eff0471b123ba7d22df9ce31aa9d25"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_3.1.5_linux_x86.tar.gz"
+      sha256 "96d76c6cac0d8f4d216131f0a51882f421e1c018a70a189d333dcb1b35638810"
 
       def install
         bin.install "veracode"
       end
     fi
     if Hardware::CPU.intel?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_1.0.48_linux_x86.tar.gz"
-      sha256 "3972e1abd3290604b82ca62da4f3b4bfe3eff0471b123ba7d22df9ce31aa9d25"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_3.1.5_linux_x86.tar.gz"
+      sha256 "96d76c6cac0d8f4d216131f0a51882f421e1c018a70a189d333dcb1b35638810"
 
       def install
         bin.install "veracode"
