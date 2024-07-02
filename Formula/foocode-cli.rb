7d42c793ae644f28623b5dff5ec649acd400dcea
Noushad <test@foocode.com>
2024-07-02 10:24:58 +0000

    Brew formula update for helloworld-cli version 2.26.0

--- Formula/foocode-cli.rb
+++ Formula/foocode-cli.rb
@@ -7,10 +7,10 @@ class FoocodeCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_macosx_arm64.tar.gz"
-      sha256 "99265aa85c0e26f2f4dc6d5c9b5fb0e102db793b16a8e4aef16e3073a7644ec4"
+      sha256 "b4cbcde7f108a1d90cce9a0f47a8f5184957b62dcdd91ccc64181cec2aa59e21"
     elsif Hardware::CPU.intel?
       url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_macosx_x86.tar.gz"
-      sha256 "b3b7e85beac14bce086db2b57f6796601c6b1c63b44ac0a861217701edf6c935"
+      sha256 "2dd13f0557726f24f88f74bedb030b8cb26a3cdd4fedfe056827f99adaeefa05"
     end
 
     def install
@@ -20,7 +20,7 @@ class FoocodeCli < Formula
 
   on_linux do
     url "https://tools.veracode.com/veracode-cli/veracode-cli_2.26.0_linux_x86.tar.gz"
-    sha256 "8ae82fbe484698d84bd9bee814ea37c44486fa558b7e3f313127514190c6a904"
+    sha256 "d44f454ad87b4179e2def0830fa1b03976e8758a68d5384f1ea260aa75edfa8e"
 
     def install
       bin.install "veracode"
